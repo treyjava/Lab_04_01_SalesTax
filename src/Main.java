6022bb25c601716732b1b171ecfd94c08b2f0ad1
Trey Burling Cincinnati <Trey@Rachels-MacBook-Air.local>
2022-09-16 12:52:10 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        int purchase = 30;
+           System.out.println("Your purchase cost " + purchase);
+           System.out.println("The sales tax for your purchase is " + purchase * .05);
+    }
+}
